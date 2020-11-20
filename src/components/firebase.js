85eaed2e9614791555e3969cb3bb356f3039290f
goldwings-ke <goldwings.events.tents@gmail.com>
2020-11-20 21:25:19 +0300

    1 file modified

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -1,4 +1,4 @@
-import firebase from 'firebase/app'
+import firebase from 'firebase'
   // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   var firebaseConfig = {
