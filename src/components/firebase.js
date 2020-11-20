f876a63fe1370eee43d399e090993d66e2a24dd2
goldwings-ke <goldwings.events.tents@gmail.com>
2020-11-20 05:37:55 +0300

    1 file modified

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -12,9 +12,10 @@ import firebase from 'firebase'
     measurementId: "G-CWGB6PH2CG"
   };
   // Initialize Firebase
+  if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
   firebase.analytics();
-
+  }
 export const provider = new firebase.auth.GoogleAuthProvider();
 export const auth = firebase.auth();
 export default firebase;
