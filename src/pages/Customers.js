d34cb84a9dea0f0876dcf7f060ce300c29610aba
goldwings-ke <goldwings.events.tents@gmail.com>
2021-01-29 06:35:54 +0300

    1 file modified

--- src/pages/Customers.js
+++ src/pages/Customers.js
@@ -493,7 +493,7 @@ class Customers extends React.Component {
           }
     }  
     return (
- <div className="w3-container" style={{width: "80%"}}>
+ <div className="w3-container" >
   <div className="w3-container w3-teal" >
     <h1>Goldwings Investments Limited</h1>
     {this.state.user ? <button onClick={this.logout}>Log Out</button>
